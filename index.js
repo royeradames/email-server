7c5c5eb4595ef976c502886cb4a7ec313ffcd1e9
royeradames <royeraadames@gmail.com>
2021-08-07 19:12:41 -0400

    📦separate front end and backend code

---
+++ index.js
@@ -0,0 +1,8 @@
+const server = require("./api/server");
+
+// dynamic port
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
+  console.log(`\n=== Server listening on port ${PORT} ===\n`)
+})
