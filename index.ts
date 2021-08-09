4304f42e9e0ab7fda8d5a61c6150af2f5cec51c7
royeradames <royeraadames@gmail.com>
2021-08-09 15:37:41 -0400

    📦listen for req in specify port

--- index.ts
+++ index.ts
@@ -4,4 +4,7 @@ import server from "./api/server"
 // allow dynamic port
 const PORT = process.env.PORT || 5000
 
-
+/* listen for req in specify port */
+server.listen(PORT, () => {
+  console.log(`\n=== Server listening on port ${PORT} ===\n`)
+})
