2aad70690bd80870ae6b7bdca957d66fe94ebabf
royeradames <royeraadames@gmail.com>
2021-08-09 17:03:50 -0400

    📦capture the validation resutls

--- helpers/handleFailValidations.ts
+++ helpers/handleFailValidations.ts
@@ -4,3 +4,14 @@ import {Response, Request, NextFunction} from "express"
 /* libraries */
 import {validationResult} from "express-validator"
 
+const handleFailtValidations = (req:Request, res:Response , next:NextFunction) => {
+    /* capture the validation resutls */
+    const error_list = validationResult(req).array()
+
+    
+
+    
+    
+
+}
+export default handleFailtValidations
