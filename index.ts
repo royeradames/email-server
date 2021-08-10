b9ac632d44b54a80b43b6d17b5a8aad1837e748d
royeradames <royeraadames@gmail.com>
2021-08-09 21:20:48 -0400

    📦only do the console log on dev env

--- index.ts
+++ index.ts
@@ -6,5 +6,6 @@ const PORT = process.env.PORT || 5000
 
 /* listen for req in specify port */
 server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`)
+  const isInDevelopmentEnviroment = process.env.PORT ? false : true
+  if(isInDevelopmentEnviroment) console.log(`\n=== Server listening on port ${PORT} ===\n`)
 })
