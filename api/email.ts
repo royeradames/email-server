73ad4531ca56966206598b41cb5b1ed7e9132e29
royeradames <royeraadames@gmail.com>
2021-08-11 13:33:23 -0400

    📦 catch send email errors👌label steps

--- api/email.ts
+++ api/email.ts
@@ -18,54 +18,41 @@ router.post("/", [
 ], 
   //if validation fails respond with error and data of what and why it fail 
   handleFailValidations, 
-  async (req:{body: {name: string, email:string, message:string}}, res:Response, next: NextFunction) => {
+ async (req:{body: {name: string, email:string, message:string}}, res:Response, next: NextFunction) => {
    /* get form data */
   const {name, email, message} = req.body
 
-  /* send email */
-  let transporter
-  let infoCapture
-  const isInDevelopmentEnviroment = process.env.PORT ? false : true
-
-  try {
-    // create reusable transporter object using the default SMTP transport
-    if (isInDevelopmentEnviroment) {
-      transporter = nodemailer.createTransport({
+  /* 1: login*/
+  let transporter = nodemailer.createTransport({
       service: "Gmail",
       auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PASS,
-      },
-      tls: {
+        },
+        tls: {
           rejectUnauthorized: false
         },
-      });
-    } else {
-        transporter = nodemailer.createTransport({
-        service: "Gmail",
-        auth: {
-          user: process.env.GMAIL_USER,
-          pass: process.env.GMAIL_PASS,
-        },
-      });
+      logger: true,
+      debug: true
+    });
+
+  /* 2: email data */
+  const mailData = {
+    from: `${name} <${email}>`, // sender address
+    to: process.env.RECEIVER_EMAIL, // list of receivers
+    subject: "Contact form", 
+    text: `email: ${email}\nmessage: ${message}\n`, // plain text body,
     }
 
-    // send mail with defined transport object
-    infoCapture = await transporter.sendMail({
-      from: `${name} <${email}>`, // sender address
-      to: process.env.RECEIVER_EMAIL, // list of receivers
-      subject: "Contact form", 
-      text: `email: ${email}\nmessage: ${message}\n`, // plain text body,
-    });
+  /* 3: send email */
+  let info
+  try {
+    info = await transporter.sendMail(mailData)
   } catch (error) {
-    next(error)
+    res.status(500).json({error})
   }
-  
-  /* info collected cannot be change */
-  const info = infoCapture
-
-  /* response with a valid status and info of success send */
   res.status(200).json(info)
+  
 })
 
 // module.exports = router
